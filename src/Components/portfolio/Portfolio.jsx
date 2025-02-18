51ac09b18950be8097427e599cecd161a48730fa
MoamenHamed32 <moamen.hamed33322@gmail.com>
2025-02-18 11:32:09 +0200

    adding some projects

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -9,6 +9,60 @@ export default function Portfolio() {
       </div>
       <div className="row w-100 mx-auto">
         <div className="col-lg-4 card">
+          <a href="https://smartstudent.live" target="_blank">
+            <div className="overlay">View Project</div>
+          </a>
+          <figure>
+            <img src="/imgs/projects/smart.png" alt="" />
+          </figure>
+        </div>
+        <div className="col-lg-4 card">
+          <a href="https://da3em.co" target="_blank">
+            <div className="overlay">View Project</div>
+          </a>
+          <figure>
+            <img src="/imgs/projects/da3em.png" alt="" />
+          </figure>
+        </div>
+
+        <div className="col-lg-4 card">
+          <a href="https://www.sedra-itqan.com/en" target="_blank">
+            <div className="overlay">View Project</div>
+          </a>
+          <figure>
+            <img src="/imgs/projects/sedra.png" alt="" />
+          </figure>
+        </div>
+      </div>
+      <div className="row w-100 mx-auto">
+        <div className="col-lg-4 card">
+          <a href="https://nibrasarab.com/" target="_blank">
+            <div className="overlay">View Project</div>
+          </a>
+          <figure>
+            <img src="/imgs/projects/nebrass.png" alt="" />
+          </figure>
+        </div>
+        <div className="col-lg-4 card">
+          <a href="https://www.autoexclecompany.com/en" target="_blank">
+            <div className="overlay">View Project</div>
+          </a>
+          <figure>
+            <img src="/imgs/projects/auto-excel.png" alt="" />
+          </figure>
+        </div>
+
+        <div className="col-lg-4 card">
+          <a href="https://hayat-ellohoum.vercel.app/en" target="_blank">
+            <div className="overlay">View Project</div>
+          </a>
+          <figure>
+            <img src="/imgs/projects/meat-life.png" alt="" />
+          </figure>
+        </div>
+      </div>
+      <div className="row w-100 mx-auto">
+        <div className="col-lg-4 card">
           <a href="https://oylimo.com" target="_blank">
             <div className="overlay">View Project</div>
           </a>
